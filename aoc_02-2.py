4d66b474bb8415d680ed4571bcfebf8efc0579cb
Rob Curtis <waab@me.com>
2023-12-02 20:01:01 -0500

    Day 2

--- aoc_02-2.py
+++ aoc_02-2.py
@@ -1,28 +1,25 @@
 input = open('aoc_02.txt', 'r')
 lines = input.readlines()
 
-max_red = 12
-max_green = 13
-max_blue = 14
-
 sum = 0
 
 for line in lines:
     parts = line.strip().split(': ')
     game_number = int(parts[0][5:])
     rounds = parts[1].split('; ')
-    possible = True
+    max_red = 0
+    max_blue = 0
+    max_green = 0
     for round in rounds:
         colors = round.split(', ')
         for color in colors:
             color_parts = color.split(' ')
             if 'red' == color_parts[1] and int(color_parts[0]) > max_red:
-                possible = False
-            elif 'blue' == color_parts[1] and int(color_parts[0]) > max_blue:
-                possible = False
+                max_red = int(color_parts[0])
+            elif 'blue' == color_parts[1]and int(color_parts[0]) > max_blue:
+                max_blue = int(color_parts[0])
             elif 'green' == color_parts[1] and int(color_parts[0]) > max_green:
-                possible = False
-    if possible:
-        print('Game {} is possible'.format(game_number))
-        sum += game_number
+                max_green = int(color_parts[0])
+    power = max_red * max_blue * max_green
+    sum += power
 print(sum)        
