29409260c82ab77c6b0e8fe011c089b932c9cfb8
Rob Curtis <waab@me.com>
2023-12-14 17:01:04 -0500

    Day 13 part 1 working

--- aoc_13-1.py
+++ aoc_13-1.py
@@ -2,12 +2,22 @@ input = open('aoc_13.txt', 'r')
 lines = input.readlines()
 
 def find_symmetry(puzzle) -> int:
-    for i in range(len(puzzle) - 1):
-        if puzzle[i] == puzzle[i+1]:
-            for j in range(1, min(i, len(puzzle) - (i+1))):
-                if puzzle[i - j] != puzzle[i + 1 + j]:
+    match = False
+    for row in range(len(puzzle) - 1):
+        if puzzle[row] == puzzle[row+1]:
+            # print('Match:\n{} : {}\n{} : {}'.format(row, puzzle[row], row + 1, puzzle[row+1]))
+            match = True
+            delta = 1
+            while row - delta >= 0 and row + 1 + delta < len(puzzle):
+                if puzzle[row - delta] != puzzle[row + 1 + delta]:
+                    # print('Broken:\n{} : {}\n{} : {}'.format(row - delta, puzzle[row - delta], row + 1 + delta, puzzle[row+1+delta]))
+                    match = False
                     break
-            return i
+                else:
+                    delta += 1
+            if match:
+                # print('Symmetry at rows {} and {}'.format(row, row+1))
+                return row + 1
     return 0
 
 total = 0
@@ -16,19 +26,20 @@ i = 0
 while i < len(lines):
     puzzle = []
     puzzle_val = 0
-    while len(lines[i].strip()) > 0:
+    while i < len(lines) and len(lines[i].strip()) > 0:
         puzzle.append(lines[i].strip())
         i += 1
     
     puzzle_val = 100 * find_symmetry(puzzle)
     
     if puzzle_val == 0:
-        # transpose 90 degrees clockwise
+        # print('No symmetry found, rotating')
         new_puz = [[puzzle[col][row] for col in range(len(puzzle)-1, -1, -1)] for row in range(len(puzzle[0]))]
         puzzle_val = find_symmetry(new_puz)
     
     total += puzzle_val
     i += 1
-    
+
+# 25369 is to low
 print(total)
     
