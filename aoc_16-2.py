33aabe2569bd4b087a30db516f9bac4335db03fe
Rob Curtis <waab@me.com>
2023-12-16 11:34:57 -0500

    Day 16 parts 1 and 2

---
+++ aoc_16-2.py
@@ -0,0 +1,75 @@
+input = open('aoc_16.txt', 'r')
+lines = [line.strip() for line in input.readlines()]
+
+def count_energized(start_point: map) -> int:
+    energized = set()
+    processed = list()
+
+    beams_to_handle = list()
+    beams_to_handle.append(start_point)
+
+    while len(beams_to_handle) > 0:
+        curr_beam = beams_to_handle.pop()
+        if curr_beam in processed:
+            continue
+        energized.add((curr_beam['row'], curr_beam['col']))
+        curr_tile = lines[curr_beam['row']][curr_beam['col']]
+        
+        if curr_beam['direction'] == 'R':
+            if curr_tile in '.-' and curr_beam['col'] + 1 < len(lines[0]):
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] + 1, 'direction': 'R'})
+            elif curr_tile == '/' and curr_beam['row'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'] - 1, 'col': curr_beam['col'], 'direction': 'U'})
+            elif curr_tile == '\\' and curr_beam['row'] + 1 < len(lines):
+                beams_to_handle.append({'row': curr_beam['row'] + 1, 'col': curr_beam['col'], 'direction': 'D'})
+            elif curr_tile == '|':
+                if curr_beam['row'] - 1 >= 0:
+                    beams_to_handle.append({'row': curr_beam['row'] - 1, 'col': curr_beam['col'], 'direction': 'U'})
+                if curr_beam['row'] + 1 < len(lines):
+                    beams_to_handle.append({'row': curr_beam['row'] + 1, 'col': curr_beam['col'], 'direction': 'D'})
+        elif curr_beam['direction'] == 'L':
+            if curr_tile in '.-' and curr_beam['col'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] - 1, 'direction': 'L'})
+            elif curr_tile == '\\' and curr_beam['row'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'] - 1, 'col': curr_beam['col'], 'direction': 'U'})
+            elif curr_tile == '/' and curr_beam['row'] + 1 < len(lines):
+                beams_to_handle.append({'row': curr_beam['row'] + 1, 'col': curr_beam['col'], 'direction': 'D'})
+            elif curr_tile == '|':
+                if curr_beam['row'] - 1 >= 0:
+                    beams_to_handle.append({'row': curr_beam['row'] - 1, 'col': curr_beam['col'], 'direction': 'U'})
+                if curr_beam['row'] + 1 < len(lines):
+                    beams_to_handle.append({'row': curr_beam['row'] + 1, 'col': curr_beam['col'], 'direction': 'D'})
+        elif curr_beam['direction'] == 'U':
+            if curr_tile in '.|' and curr_beam['row'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'] - 1, 'col': curr_beam['col'], 'direction': 'U'})
+            elif curr_tile == '/' and curr_beam['col'] + 1 < len(lines[0]):
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] + 1, 'direction': 'R'})
+            elif curr_tile == '\\' and curr_beam['col'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] - 1, 'direction': 'L'})
+            elif curr_tile == '-':
+                if curr_beam['col'] - 1 >= 0:
+                    beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] - 1, 'direction': 'L'})
+                if curr_beam['col'] + 1 < len(lines[0]):
+                    beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] + 1, 'direction': 'R'})
+        elif curr_beam['direction'] == 'D':
+            if curr_tile in '.|' and curr_beam['row'] + 1 < len(lines):
+                beams_to_handle.append({'row': curr_beam['row'] + 1, 'col': curr_beam['col'], 'direction': 'D'})
+            elif curr_tile == '\\' and curr_beam['col'] + 1 < len(lines[0]):
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] + 1, 'direction': 'R'})
+            elif curr_tile == '/' and curr_beam['col'] - 1 >= 0:
+                beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] - 1, 'direction': 'L'})
+            elif curr_tile == '-':
+                if curr_beam['col'] - 1 >= 0:
+                    beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] - 1, 'direction': 'L'})
+                if curr_beam['col'] + 1 < len(lines[0]):
+                    beams_to_handle.append({'row': curr_beam['row'], 'col': curr_beam['col'] + 1, 'direction': 'R'})
+        processed.append(curr_beam)
+
+    return(len(energized))
+
+top_starts = [count_energized({'row':0,'col':i, 'direction':'D'}) for i in range(len(lines[0]))]
+left_starts = [count_energized({'row':i,'col':0, 'direction':'R'}) for i in range(len(lines))]
+right_starts = [count_energized({'row':i,'col':len(lines[0]) - 1, 'direction':'L'}) for i in range(len(lines))]
+bottom_starts = [count_energized({'row':len(lines) - 1,'col':i, 'direction':'U'}) for i in range(len(lines[0]))]
+
+print(max([max(top_starts), max(left_starts), max(right_starts), max(bottom_starts)]))
